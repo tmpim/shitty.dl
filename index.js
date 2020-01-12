2b80b4f70afeeaab09ea4fe5fcc9e445fd05d698
Drew Lemmy <drew@lemmmy.pw>
2020-01-12 14:06:52 +0000

    Redirect to raw paste for certain useragents

--- index.js
+++ index.js
@@ -565,7 +565,13 @@ router.post("/edit", (req, res) => {
 	});
 });
 
+function shouldReturnRaw(req) {
+	return config.rawPasteAgents && new RegExp(config.rawPasteAgents, "i").test(req.headers["user-agent"]);
+}
+
 router.get("/paste/:file", (req, res) => {
+	if (shouldReturnRaw(req)) return res.redirect("/" + req.params.file);
+
 	const filename = sanitizeFilename(req.params.file);
 	const filePath = path.join(config.imagePath, filename);
 
@@ -597,6 +603,8 @@ router.get("/paste/:file", (req, res) => {
 });
 
 router.get("/edit/:file", (req, res) => {
+	if (shouldReturnRaw(req)) return res.redirect("/" + req.params.file);
+
 	let editor = true;
 	if (!req.session || !req.session.authed) {
 		if (!req.body.password) editor = undefined;
